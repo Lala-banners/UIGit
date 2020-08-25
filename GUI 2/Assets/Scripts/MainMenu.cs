808db84a1953548ddf0a1cd652fe12e023c88959
Lala-banners <lala.l.banno@hotmail.com>
2020-08-25 19:42:23 +1000

    First Commit
    Final Project

---
+++ GUI 2/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour
+// Public Class is the name of your Script 
+// Monobehaviour is the base class for every unity script made
+{
+    public void ChangeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    #region Public variables
+    //Allows you to close and open text under name
+    public string LoadScene = "GameScene";
+    //Represents text as a series of characters
+    //string is allias for system string
+    public Dropdown qualityDropdown;
+    //Shows the different quality options in options menu
+    public Toggle fullscreenToggle;
+    //Allows you to toggle between fullscreen and not fullscreen
+    public GameObject IWantToDisableThis;
+    //Disables game object that it's attached to
+    public void Quality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetFullScreen(bool isFullScreen) //This changes the screen from fullscreen to windowed
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public Resolution[] resolutions;
+    public Dropdown resolution;
+
+    #endregion
+    public void Start()
+    //Public Void is Name for group of code 
+    {
+        Debug.Log("Starting Game Main Menu");
+        //Sends a command to unity 
+        if (!PlayerPrefs.HasKey("fullscreen"))
+        //Saves player preferences between game sessions
+        {
+            PlayerPrefs.SetInt("fullscreen", 0);
+            Screen.fullScreen = false;
+        }
+        else
+        {
+            if (PlayerPrefs.GetInt("fullscreen") == 0)
+            {
+                Screen.fullScreen = false;
+            }
+            else
+            {
+                Screen.fullScreen = true;
+            }
+        }
+        if (!PlayerPrefs.HasKey("quality"))
+        {
+            PlayerPrefs.SetInt("quality", 5);//dont have magic numbers
+            QualitySettings.SetQualityLevel(5);
+        }
+        else
+        {
+            QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("quality"));
+        }
+        PlayerPrefs.Save();
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    #region Change settings
+
+    private void Start2()//This is for setting resolution
+    {
+        resolutions = Screen.resolutions;
+        resolution.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[1].height;
+            options.Add(option);
+            if(resolutions[i].width == Screen.currentResolution.width && 
+               resolutions[1].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolution.AddOptions(options);
+        resolution.value = currentResolutionIndex;
+        resolution.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = resolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+
+    #endregion
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
+        Application.Quit();
+    }
+    #region Save and load player prefs
+    public void SavePlayerPrefs()
+    {
+        PlayerPrefs.SetInt("quality", QualitySettings.GetQualityLevel());
+        //PlayerPrefs.SetInt("qualityDropdown.value);
+        if (fullscreenToggle.isOn)
+        {
+            PlayerPrefs.SetInt("fullscreen", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("fullscreen", 0);
+        }
+        PlayerPrefs.Save();
+    }
+    public void LoadPlayerPrefs()
+    {
+        qualityDropdown.value = PlayerPrefs.GetInt("quality");
+        if (PlayerPrefs.GetInt("fullscreen") == 0)
+        {
+            fullscreenToggle.isOn = false;
+        }
+        else
+        {
+            fullscreenToggle.isOn = true;
+        }
+    }
+    #endregion
+}
+
+
+
+
+
---
+++ GUI 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    //mouse input
+    ////rotate the camera up and down
+    ////rotate the character left and right
+    //keyboard input
+    ////move the character
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    private Vector3 velocity;
+    public float mouseSensitivity = 100f;
+    private float xRotation = 0f;
+    Camera camera;
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        camera = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        MouseLook();
+        Move();
+    }
+    private void MouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+    private void Move()
+    {
+        //x = -1 to 1
+        float x = Input.GetAxis("Horizontal");
+        //z = -1 to 1
+        float z = Input.GetAxis("Vertical");
+        //we want to move in this direction
+        Vector3 move = (transform.right * x) + (transform.forward * z);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move((velocity + move) * speed * Time.deltaTime);
+    }
+}
+
+
+
+
+
